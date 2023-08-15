8e15ac99a00b6a8231dcbced03719ca7c4602ca7
yu00hun <yu00hun@naver.com>
2023-08-15 14:26:37 +0900

    새 메모 쓰기 화면

---
+++ SwiftMemoProject/ComposeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComposeViewController.swift
+//  SwiftMemoProject
+//
+//  Created by 유훈 on 2023/08/15.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
