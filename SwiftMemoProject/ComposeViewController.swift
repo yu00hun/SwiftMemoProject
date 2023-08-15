e3dd8bb2cf9169de5bd0c30ad189f643cfcbbbc4
yu00hun <yu00hun@naver.com>
2023-08-15 14:46:33 +0900

    취소 기능 구현

--- SwiftMemoProject/ComposeViewController.swift
+++ SwiftMemoProject/ComposeViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
-
+    
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
