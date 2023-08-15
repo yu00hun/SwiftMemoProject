9e3963526b50a4844e6fc2872f81606d3e8e25ce
yu00hun <yu00hun@naver.com>
2023-08-15 14:56:59 +0900

    메모 저장 구현

--- SwiftMemoProject/ComposeViewController.swift
+++ SwiftMemoProject/ComposeViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
-    
     @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var memoTextView: UITextView!
+    
+    @IBAction func save(_ sender: Any) {
+        guard let memo = memoTextView.text, memo.count > 0 else {
+            alert(message: "메모를 입력하세요")
+            return
+        }
+        
+        let newMemo = Memo(content: memo)
+        Memo.dummyMemoList.append(newMemo)
+        
         dismiss(animated: true, completion: nil)
     }
     
---
+++ SwiftMemoProject/UIViewController+Alert.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Alert.swift
+//  SwiftMemoProject
+//
+//  Created by 유훈 on 2023/08/15.
+//
+
+import UIKit
+
+extension UIViewController {
+    func alert(title: String = "알림", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
